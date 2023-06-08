6bfa6ede6c7ce3651f9b3bfc5706d69ba4276d8c
Ayush Patel <115834787+ayushpatel2307@users.noreply.github.com>
2023-06-09 00:14:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ form.addEventListener('submit', (event) => {
 	const description = document.getElementById('description').value;
 	const contactName = document.getElementById('contact-name').value;
 	const contactEmail = document.getElementById('contact-email').value;
-
+	const itemdate = document.getElementById('item-date').value;
+	const itemlocation = document.getElementById('item-location').value;
+	const itemtime = document.getElementById('item-time').value;
+	const phonenumber = document.getElementById('phone-number').value;
 	// TODO: Send form data to server or do something with it
 
 	alert('Form submitted successfully');
